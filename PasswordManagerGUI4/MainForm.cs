b6dfbd4fc4ef06c967d3ce1914076bd09855c3e2
Ali Razi <95105244+alirazi1992@users.noreply.github.com>
2025-09-16 01:04:11 -0700

    Add files via upload

---
+++ PasswordManagerGUI4/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PasswordManagerGUI4
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // MessageBox.Show("MainForm loaded");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) { }
+        private void btnDelete_Click(object sender, EventArgs e) { }
+        private void btnReveal_Click(object sender, EventArgs e) { }
+    }
+}
